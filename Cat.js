84284538db56a8f815e3e4a455ef28a44cb44060
Chao Legion <chaolegons@gmail.com>
2019-08-27 21:09:48 +0700

    revert method eat for Cat

--- Cat.js
+++ Cat.js
@@ -1,4 +1,5 @@
 function Cat(){
+<<<<<<< HEAD
   this.stomath = [];
   this.age = 0;
 }
@@ -9,4 +10,9 @@ Cat.prototype.eat = function(food){
 Cat.prototype.addAge = function(age){
   this.age = age
 }
+=======
+
+}
+
+>>>>>>> parent of 979dda3... add method eat for Cat.js
 module.export = Cat;

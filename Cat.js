b86b91e178b6a9d1d47287722f9db97d49b536b4
Chao Legion <chaolegons@gmail.com>
2019-08-27 21:15:19 +0700

    modified Cat and add init npm

--- Cat.js
+++ Cat.js
@@ -1,5 +1,4 @@
 function Cat(){
-<<<<<<< HEAD
   this.stomath = [];
   this.age = 0;
 }
@@ -10,9 +9,4 @@ Cat.prototype.eat = function(food){
 Cat.prototype.addAge = function(age){
   this.age = age
 }
-=======
-
-}
-
->>>>>>> parent of 979dda3... add method eat for Cat.js
 module.export = Cat;

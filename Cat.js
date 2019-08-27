c0f4d858e9a30be7a6a08fed9276da48aeed3675
Chao Legion <chaolegons@gmail.com>
2019-08-27 21:03:08 +0700

    add method Age for Cat

--- Cat.js
+++ Cat.js
@@ -1,7 +1,12 @@
 function Cat(){
-  this.stomath = []
+  this.stomath = [];
+  this.age = 0;
 }
 Cat.prototype.eat = function(food){
   this.stomath.push(food);
+}
+
+Cat.prototype.addAge = function(age){
+  this.age = age
 }
 module.export = Cat;

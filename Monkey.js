73496dd2df25d22fcd964f67170b6be7c32d8de6
Ip ZeroKID <chaolegons@gmail.com>
2019-09-01 15:34:06 +0700

    remove function for Monkey

--- Monkey.js
+++ Monkey.js
@@ -2,11 +2,4 @@ function Monkey(){
 
 }
 
-Monkey.prototype.jump = function(){
-  console.log('Jumping !!!');
-}
-Monkey.prototype.run = function(){
-  console.log('Running...');
-
-}
 module.exports = Monkey;
